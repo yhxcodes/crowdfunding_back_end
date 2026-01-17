4a27ad7fada98bbfd02c1f4e660d78cbfbe1a0b9
Sophie Wong <moogi27@gmail.com>
2026-01-17 11:49:18 +1100

    wrote our first DRF serializer

---
+++ crowdfunding/fundraisers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from django.apps import apps 
+
+class FundraiserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('fundraisers.Fundraiser')
+        fields = '__all__'
