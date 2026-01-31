e142ea4e5541a40205e3b66d9fffb1a513364934
Sophie Wong <moogi27@gmail.com>
2026-01-31 13:03:27 +1100

    added in permissions

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -3,13 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView 
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status, permissions
 from django.http import Http404
 from rest_framework.generics import get_object_or_404
 from .models import Fundraiser, Pledge
+from .permissions import IsOwnerOrReadOnly
 # from .serializers import FundraiserSerializer, PledgeSerializer
 from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer
+
 class FundraiserList(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+        ]
 
     def get(self, request):
         fundraisers = Fundraiser.objects.all()
@@ -30,11 +36,34 @@ class FundraiserList(APIView):
         )
 
 class FundraiserDetail(APIView):
+permission_classes = [
+    permissions.IsAuthenticatedOrReadOnly,
+    IsOwnerOrReadOnly
+]
+
     def get(self, request, pk):
         fundraiser = get_object_or_404(Fundraiser, pk=pk)
         # serializer = FundraiserSerializer(fundraiser)
         serializer = FundraiserDetailSerializer(fundraiser)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        fundraiser = self.get_object_or_404(fundraiser, pk=pk)
+        self.check_object_permissions(request, fundraiser)
+        serializer = FundraiserDetailSerializer(
+            instance=fundraiser,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
  
 class PledgeList(APIView):
 
